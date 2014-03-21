32301cd86b0497d90a3353d13f869ae120eb6c98
MeatwadGER <meatwad.ger@gmail.com>
2014-03-21 22:36:52 +0100

    sdk project crated

--- gen/uni/ma/todotogo/BuildConfig.java
+++ gen/uni/ma/todotogo/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.uni.ma.todotogo;
+package uni.ma.todotogo;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
