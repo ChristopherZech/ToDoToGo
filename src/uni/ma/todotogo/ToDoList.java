0639d8160048e582bb257a5d71e1a2b84a7a8324
MeatwadGER <meatwad.ger@gmail.com>
2014-03-21 23:57:45 +0100

    added todo list view

--- src/uni/ma/todotogo/ToDoList.java
+++ src/uni/ma/todotogo/ToDoList.java
@@ -1,8 +1,14 @@
 package uni.ma.todotogo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class ToDoList extends Activity {
 
@@ -10,6 +16,17 @@ public class ToDoList extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_to_do_list);
+		
+		List valueList = new ArrayList<String>();
+		for (int i = 0; i < 20; i++)
+		{
+			valueList.add("value"+i);
+		}
+		
+		ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+		final ListView lv = (ListView)findViewById(R.id.todolist);
+
+		lv.setAdapter(adapter);
 	}
 
 	@Override
