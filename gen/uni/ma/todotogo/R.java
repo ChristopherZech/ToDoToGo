7ad490be619298f1c92b63c31f3a5e40edec6dcf
MeatwadGER <meatwad.ger@gmail.com>
2014-03-23 16:39:56 +0100

    finalized list view

--- gen/uni/ma/todotogo/R.java
+++ gen/uni/ma/todotogo/R.java
@@ -21,15 +21,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_new=0x7f020000;
+        public static final int ic_action_settings=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_add=0x7f080002;
-        public static final int action_settings=0x7f080001;
+        public static final int action_add=0x7f080004;
+        public static final int action_settings=0x7f080003;
+        public static final int distance=0x7f080002;
+        public static final int tododesc=0x7f080001;
         public static final int todolist=0x7f080000;
     }
     public static final class layout {
         public static final int activity_to_do_list=0x7f030000;
+        public static final int layout_todo_listentry=0x7f030001;
     }
     public static final class menu {
         public static final int to_do_list=0x7f070000;
--- src/uni/ma/todotogo/ToDoList.java
+++ src/uni/ma/todotogo/ToDoList.java
@@ -1,14 +1,19 @@
 package uni.ma.todotogo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 public class ToDoList extends Activity {
 
@@ -17,15 +22,23 @@ public class ToDoList extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_to_do_list);
 		
-		List valueList = new ArrayList<String>();
+		// fill the test list with content
+		ArrayList<HashMap<String, Object>> myList = new ArrayList<HashMap<String,Object>>();
 		for (int i = 0; i < 20; i++)
 		{
-			valueList.add("value"+i);
+			myList.add(addItem("ToDoName", "Entry Item "+i, "distance", ""+i+"m"));
+			//valueList1.add("value 1");
+			//valueList2.add(i);
 		}
+		//ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+		ListView lv = (ListView)findViewById(R.id.todolist);
 		
-		ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
-		final ListView lv = (ListView)findViewById(R.id.todolist);
-
+		SimpleAdapter adapter = new SimpleAdapter(this, myList, 
+                R.layout.layout_todo_listentry,
+                new String[]{"ToDoName", "distance"}, 
+                new int[]{R.id.tododesc, R.id.distance}
+            );
+		
 		lv.setAdapter(adapter);
 	}
 
@@ -35,5 +48,14 @@ public class ToDoList extends Activity {
 		getMenuInflater().inflate(R.menu.to_do_list, menu);
 		return true;
 	}
+	
+    public HashMap<String, Object> addItem(String key1, String value1, String key2, String value2){
+        HashMap<String, Object> item = new HashMap<String, Object>();
+        
+        item.put(key1, value1);
+        item.put(key2, value2);
+        
+        return item;
+    }
 
 }
