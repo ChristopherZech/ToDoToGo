4875563f52555f5c6e9fa66655930cf091f9d0b7
slseiter <simon.seiter@gmx.de>
2014-04-02 13:55:04 +0200

    Changed icon

--- gen/uni/ma/todotogo/R.java
+++ gen/uni/ma/todotogo/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_action_new=0x7f020000;
-        public static final int ic_action_settings=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_action_name=0x7f020000;
+        public static final int ic_action_new=0x7f020001;
+        public static final int ic_action_settings=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int action_add=0x7f0a0009;
